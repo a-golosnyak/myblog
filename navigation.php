09c0e74daea1927d1d2bd963b0688143ba4a9796
AND <andrey_g.pt@mail.ru>
2018-07-05 17:49:38 +0300

    sign in navi dropdown menu size doesnt work.

--- navigation.php
+++ navigation.php
@@ -41,37 +41,78 @@
                 <div class="nav nav-tabs">
                     <div class="nav-item dropdown " style="left: auto; right: 0;">
 
-                        <?php 
+                        <?php
+                        require_once  'log.php' ; 
+
                         if ($userLoggedIn == true) 
                         {
                             echo    "<div class='media-middle nav-item'>
                                         <img class='avatar'src='images/ava/avamin.jpg'alt='...'>
                                     </div>
 
-                                    <div class='nav-link dropdown-toggle nav-item offset-sm-0'data-toggle='dropdown'href='#'role='button'    aria-haspopup='true'aria-expanded='false'>
+                                    <div class='nav-link dropdown-toggle nav-item user_nav_item'data-toggle='dropdown'href='#'role='button'    aria-haspopup='true'aria-expanded='false'>
                                     </div>
                                     <div class='dropdown-menu dropdown-menu-right'>
-                                        <div class='dropdown-item'href='#'>Action</div>
+                                        <div class='dropdown-item'href='#'>Вы вошли как $userstr</div>
                                         <div class='dropdown-divider'></div>
-                                        <div class='dropdown-item'href='#'>Another action</div>
-                                        <div class='dropdown-item'href='#'>Something else here</div>
-                                        <div class='dropdown-item'href='#'>Separated link</div>
+                                        <div class='dropdown-item'href='#'>Профиль</div>
+                                        <div class='dropdown-divider'></div>
+                                        <div class='dropdown-item'href='#'>Выход</div>
                                     </div>";
                         } 
                         else 
                         { 
-                            echo    "<div class='nav-link dropdown-toggle' data-toggle='dropdown' href='#'' role='button'         aria-haspopup='true' aria-expanded='false'>Sign In</div>
-                                        <div class='dropdown-menu dropdown-menu-right'>
-                                            <div class='dropdown-item' href='#'>Action</div>
-                                            <div class='dropdown-divider'></div>
-                                            <div class='dropdown-item' href='#'>Another action</div>
-                                            <div class='dropdown-item' href='#'>Something else here</div>
-                                            <div class='dropdown-item' href='#'>Separated link</div>
-                                        </div> ";
+                            echo    "<div class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>Sign In
+                                </div>
+                                <div class='dropdown-menu dropdown-menu-right'>
+                                    <div class='dropdown-item' href='#'>
+                                        <label for='inputEmail' class='sr-only'></label>
+                                        <input type='email' id='inputEmail' maxlength='30' size='20' class='form-control' placeholder='Email address' required autofocus>
+                                    </div>
+
+                                <form class='form-signin'>
+                                    <div class='dropdown-item' href='#'>
+                                        <label for='inputPassword' class='sr-only'>Password</label>
+                                        <input type='password' id='inputPassword' class='form-control' size='40' placeholder='Password' required>
+                                    </div>
+                                    <div class='dropdown-item' href='#'>
+                                        <div class='checkbox '>
+                                            <label>
+                                                <input type='checkbox' value='remember-me'> Remember me
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class='dropdown-item' href='#'>
+                                        <button class='btn btn-lg btn-primary btn-block' type='submit'>Sign in</button>
+                                    </div>
+                                    <div class='dropdown-divider'></div>
+                                    <div class='dropdown-item' href='#'>
+                                        <button class='btn btn-md btn-primary btn-block' type='submit'>Регистрация</button>
+                                    </div>
+                                </form>
+                            </div> 
+                                ";
 
                         }       
                     ?>      
-                                 
+
+                            
+                       
+
+  <!--                  <form class="form-signin">
+                        <h2 class="form-signin-heading">Please sign in</h2>
+                        <label for="inputEmail" class="sr-only">Email address</label>
+                        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                        <label for="inputPassword" class="sr-only">Password</label>
+                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                        <div class="checkbox">
+                        <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                        </label>
+                        </div>
+                        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                    </form>
+-->
                     </div>
                 </div> 
             </div>
