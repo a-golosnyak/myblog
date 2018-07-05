1a87aa837327b66760800384ef42fbb57076ef12
AND <andrey_g.pt@mail.ru>
2018-07-05 22:14:16 +0300

    new files

---
+++ log.php
@@ -0,0 +1,13 @@
+<?php // Example 26-7: login.php
+$userstr = ' (Guest)';
+
+    if (isset($_SESSION['user']))
+    {
+        $user     = $_SESSION['user'];
+        $userLoggedIn = TRUE;
+        $userstr  = " ($user)";
+    }
+    else 
+        $userLoggedIn = FALSE;
+?>
+
