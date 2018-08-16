af10a7da59722b84f9996faf3eb4cdea6aa3d516
AND <andrey_g.pt@mail.ru>
2018-08-16 17:55:13 +0300

    Text editor added!

--- profile.php
+++ profile.php
@@ -201,7 +201,19 @@
                         <div style="clear: both;"> </div>
 
 
-                        <!-- <img src='$user.jpg' style='float:left;'> -->        
+                        <!-- <img src='$user.jpg' style='float:left;'> -->  
+
+                        <form>
+                            <textarea name="editor1" id="editor1" rows="10" cols="80">
+                                This is my textarea to be replaced with CKEditor.
+                            </textarea>
+                            <script>
+                                // Replace the <textarea id="editor1"> with a CKEditor
+                                // instance, using default configuration.
+                                CKEDITOR.replace( 'editor1' );
+                            </script>
+                        </form>
+              
                     </div>
                 </div><!-- /.blog-main -->
 
