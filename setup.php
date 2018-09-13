f1f915d4b48c86ace22d18ab2bedf6a6e3f2612a
AND <andrey_g.pt@mail.ru>
2018-09-13 10:25:57 +0300

    Database encoding set to utf8

--- setup.php
+++ setup.php
@@ -18,7 +18,7 @@
         
         if(!$db_selected)
         {
-            $query = "CREATE DATABASE test_blog_db";  
+            $query = "CREATE DATABASE test_blog_db DEFAULT CHARACTER SET utf8";  
             $result = mysqli_query($link, $query);                  // Запрос без создания сущности
             echo "Database test_blog_db created.";
         }
